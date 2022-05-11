ef83342b2a30883e08781a76af5bd036745ca757
Lior Grishin <94124882+lorGR@users.noreply.github.com>
2022-05-11 16:18:36 +0300

    sa

--- Node/01_Joke_HW/Simple/dist/server.js
+++ Node/01_Joke_HW/Simple/dist/server.js
@@ -1,10 +1,7 @@
-var express = require('epxress');
+var express = require('express');
 var app = express();
 var port = process.env.port || 3000;
-app.use(express.static('public'));
-app.get('/', function (req, res) {
-    res.send('hi');
-});
-app.listen(port, function () {
-    console.log("LISTENING ON PORT " + port);
+var path;
+app.listen(port(), {
+    console: console, : .log("LISTENING ON PORT " + port)
 });
--- Node/01_Joke_HW/Simple/server.ts
+++ Node/01_Joke_HW/Simple/server.ts
@@ -1,13 +1,8 @@
-const express = require('epxress');
+const express = require('express');
 const app = express();
 const port = process.env.port || 3000;
+const path
 
-app.use(express.static('public'));
-
-app.get('/', (req, res)=> {
-    res.send('hi');
-})
-
-app.listen(port, ()=>{
+app.listen(port ()=> {
     console.log(`LISTENING ON PORT ${port}`);
 })
